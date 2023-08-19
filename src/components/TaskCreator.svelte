c76f244d63c4d8846dd98796daba054ad3e1efff
Hennadii Bohdanov <soulstice.web@gmail.com>
2023-08-20 00:05:08 +0200

    Improve task creation usability
    - Input box value gets cleared after task creation
    - Pressing "Enter" now has the same behavior as clicking on "Add" button
    - Disable auto-completion

--- src/components/TaskCreator.svelte
+++ src/components/TaskCreator.svelte
@@ -15,6 +15,7 @@
 			'date': Date.now(),
 		};
 
+		currentText = ""
 		saveInStorage(newTask)
 		$tasks.unshift(newTask)
 		$tasks = $tasks
@@ -24,12 +25,23 @@
 	function saveInStorage(task) {
 		localStorage.setItem(task.id, JSON.stringify(task))
 	}
+
+	function onKeyDown(e) {
+		{
+			switch(e.keyCode) {
+				case 13:
+					addTask()
+					break;
+			}
+		}
+	}
 </script>
 
 
 <div class="flex justify-center">
 	<div class="mb-0 w-full">
-		<input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal bg-clip-padding border border-solid rounded ease-in-out m-0 focus:border-blue-600 focus:outline-none bg-slate-600 focus:bg-slate-700 text-white focus:text-gray-200 border-slate-500" id="exampleFormControlInput1" placeholder="Enter a task" bind:value={currentText}>
+		<input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal bg-clip-padding border border-solid rounded ease-in-out m-0 focus:border-blue-600 focus:outline-none bg-slate-600 focus:bg-slate-700 text-white focus:text-gray-200 border-slate-500" id="exampleFormControlInput1" placeholder="Enter a task" autocomplete="off" bind:value={currentText} on:keydown={(e) => {onKeyDown(e)}
+		}>
 	</div>
 	<div class="flex space-x-2 justify-center pl-1">
 		<button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out !outline-none" on:click={() => addTask()}>
