479c8b8d3c5b0ad781707571f9b483b03ef6717a
Hennadii Bohdanov <soulstice.web@gmail.com>
2023-08-19 16:08:26 +0200

    Move header into a separate component

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,10 @@
 <script>
 import Task from "./components/Task.svelte";
+import Header from "./components/Header.svelte";
 </script>
 
 <main class="min-h-screen pt-4 md:pt-4 pb-2 px-4 md:px-0 body-bg-dark">
-  <header class="max-w-lg mx-auto flex flex-row pr-3 pl-3 pt-2 pb-1">
-    <h1 class="text-2xl font-bold text-white flex-1">To-Do App</h1>
-    <button type="button" class="inline-block rounded border-2 bg-slate-800 border-gray-700 text-white leading-normal uppercase shadow-md hover:bg-slate-800 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg ease-in-out w-9 h-9 !outline-none"><svg viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 mr-2" style="margin: auto;">
-      <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" class="stroke-white dark:stroke-white"></path>
-      <path d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12
-			20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836" class="stroke-white dark:stroke-white"></path>
-    </svg></button>
-  </header>
+  <Header></Header>
 
   <div class="max-w-lg mx-auto p-3 md:p-3 my-4 rounded-lg shadow-2xl bg-slate-900">
     <section>
