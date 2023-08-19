fd64c2d5afa720d1b0bf489abe8322165ff6e30e
Hennadii Bohdanov <soulstice.web@gmail.com>
2023-08-19 16:05:58 +0200

    Create basic app layout

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,34 @@
 import Task from "./components/Task.svelte";
 </script>
 
-<main>
-  <div class="container mx-auto">
-    <h1 class="text-2xl font-bold">
-      Svelte To-Do App
-    </h1>
-    <div class="col">
-      <Task text="Task 1"></Task>
-      <Task text="Task 2"></Task>
-      <Task text="Task 3"></Task>
-    </div>
+<main class="min-h-screen pt-4 md:pt-4 pb-2 px-4 md:px-0 body-bg-dark">
+  <header class="max-w-lg mx-auto flex flex-row pr-3 pl-3 pt-2 pb-1">
+    <h1 class="text-2xl font-bold text-white flex-1">To-Do App</h1>
+    <button type="button" class="inline-block rounded border-2 bg-slate-800 border-gray-700 text-white leading-normal uppercase shadow-md hover:bg-slate-800 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg ease-in-out w-9 h-9 !outline-none"><svg viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-6 h-6 mr-2" style="margin: auto;">
+      <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" class="stroke-white dark:stroke-white"></path>
+      <path d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12
+			20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836" class="stroke-white dark:stroke-white"></path>
+    </svg></button>
+  </header>
+
+  <div class="max-w-lg mx-auto p-3 md:p-3 my-4 rounded-lg shadow-2xl bg-slate-900">
+    <section>
+      <div class="flex justify-center">
+        <div class="mb-0 w-full">
+          <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal bg-clip-padding border border-solid rounded ease-in-out m-0 focus:border-blue-600 focus:outline-none bg-slate-600 focus:bg-slate-700 text-white focus:text-gray-200 border-slate-500" id="exampleFormControlInput1" placeholder="Enter a task">
+        </div>
+        <div class="flex space-x-2 justify-center pl-1">
+          <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out !outline-none">
+            Add
+          </button>
+        </div>
+      </div>
+
+      <section id="task-list">
+        <Task text="Task 1"></Task>
+        <Task text="Task 2" isCompleted={true}></Task>
+        <Task text="Task 3"></Task></section>
+    </section>
   </div>
+
 </main>
--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -1,9 +1,46 @@
 <script>
 	export let text = ""
+	export let isCompleted = false
 </script>
 
-<main>
-	<div class="row">
-		{text}
+<div class="flex w-full">
+	<div class="flex flex-row mt-2 p-4 rounded-lg border
+
+		bg-gray-800 border-gray-700
+
+		 form-check w-full">
+
+		<input class="
+		  form-check-input appearance-none h-4 w-4 border rounded-sm
+
+      checked:after:m-[0.1rem] checked:after:ml-[0.275rem]
+      checked:after:block checked:after:h-[0.5rem] checked:after:w-[0.35rem]
+      checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0
+      checked:after:border-l-0
+
+		  bg-slate-600 checked:bg-green-700 checked:border-green-600 border-slate-500
+
+		  focus:outline-none
+		  align-top bg-no-repeat bg-center bg-contain float-left mr-3 ml-1 cursor-pointer scale-150 mt-auto mb-auto" type="checkbox" value="" style="
+
+		  " bind:checked={isCompleted}>
+		<span class=" inline-block pl-1 pr-2
+
+		  text-gray-400
+
+		  flex-1 mt-auto mb-auto wrap-anywhere">
+			{text}
+		</span>
+		<button type="button" class="mt-auto mb-auto
+		  bg-gray-900 text-gray-400 hover:bg-gray-700 hover:text-white
+
+		  inline-block rounded-full leading-normal shadow-md
+		  hover:shadow-lg focus:shadow-lg focus:outline-none border-none !outline-none focus:ring-0
+		  active:shadow-lg transition duration-150 ease-in-out w-9 h-9">
+			<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 12 16" height="16px" width="16px" xmlns="http://www.w3.org/2000/svg" style="margin: auto;">
+				<path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6
+		  6.52l3.75-3.75 1.48 1.48L7.48 8z" class=""></path></svg>
+		</button>
+
 	</div>
-</main>
+</div>
