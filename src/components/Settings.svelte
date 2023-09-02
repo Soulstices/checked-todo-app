3c3300154d598b98704bfd23d3d355e73fd6d561
Hennadii Bohdanov <soulstice.web@gmail.com>
2023-09-02 22:56:55 +0200

    Refactor Settings.svelte

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import Modal from './Modal.svelte'
 	import ThemePicker from './ThemePicker.svelte'
+
+	const themes: string[] = ['dark-blue', 'light']
 </script>
 
 <Modal titleIcon="settings" title="Settings">
 	<p class="text-modal font-medium text-lg pt-5">Theme</p>
 	<div class="flex flex-col md:flex-row items-center text-lg font-bold pt-1 gap-4">
-		<ThemePicker newTheme="dark-blue" />
-		<ThemePicker newTheme="light" />
+		{#each themes as theme}
+			<ThemePicker newTheme={theme} />
+		{/each}
 	</div>
 </Modal>
