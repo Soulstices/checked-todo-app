1abed8a43aaf74b6445f7276bdba684163a12c69
Hennadii Bohdanov <soulstice.web@gmail.com>
2024-01-22 20:05:38 +0100

    Implement scroll to top button

--- src/components/StickyBar.svelte
+++ src/components/StickyBar.svelte
@@ -2,6 +2,7 @@
 	import { onMount, onDestroy } from 'svelte';
 	import { tasks } from '../lib/store.js'
 
+	let isAtTop: boolean = true;
 	let completedCount: number;
 	let hasScrollbar: boolean = false;
 
@@ -21,10 +22,19 @@
 		checkScrollbar();
 	}
 
+	function scrollToTop(): void {
+		window.scroll(0, 0);
+	}
+
+	function updateScrollPosition() {
+		isAtTop = window.scrollY < 100;
+	}
+
 	onMount(() => {
 		checkScrollbar();
 		window.addEventListener('resize', handleResize);
 		document.body.addEventListener('DOMSubtreeModified', handleContentChange);
+		window.addEventListener('scroll', updateScrollPosition);
 	});
 
 	onDestroy(() => {
@@ -34,8 +44,13 @@
 </script>
 
 {#if hasScrollbar}
-	<div class="fixed bottom-0 bg-container border-task border-t flex w-full items-center z-10 py-4">
-		<div class="m-auto text-task">
+	<div class="fixed bottom-0 bg-container border-task border-t flex w-full items-center z-10 py-4 text-task">
+		<button on:click={scrollToTop} class="absolute left-4 bg-themepicker duration-200 px-2 h-8 w-8 rounded" class:hidden={isAtTop}>
+			<svg data-slot="icon" fill="none" stroke-width="6" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+				<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"></path>
+			</svg>
+		</button>
+		<div class="m-auto">
 			<span class="font-bold">Tasks:</span>
 			<span class:text-task-completed={completedCount === $tasks.length} class:font-bold={completedCount === $tasks.length}>
 				{completedCount}/{$tasks.length}
