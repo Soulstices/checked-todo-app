d7686f0ba62e756ec5a8f5a8fb79780345f409c2
Hennadii Bohdanov <soulstice.web@gmail.com>
2024-01-23 16:08:34 +0100

    Adjust positioning of buttons inside a sticky bar

--- src/components/StickyBar.svelte
+++ src/components/StickyBar.svelte
@@ -86,7 +86,7 @@
 
 {#if $tasks.length > 0 && $activeTabIndex === 0}
 	<div class="fixed bottom-0 bg-container left-1/2 translate-x-[-50%] border-task  rounded-t flex w-full items-center z-10 py-4 text-task max-w-lg">
-		<button on:click={scrollToTop} class="absolute left-4 bg-themepicker duration-200 px-2 h-8 w-8 rounded" class:hidden={isAtTop}>
+		<button on:click={scrollToTop} class="absolute left-3 bg-themepicker duration-200 px-2 h-8 w-8 rounded" class:hidden={isAtTop}>
 			<svg data-slot="icon" fill="none" stroke-width="6" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 				<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"></path>
 			</svg>
@@ -97,7 +97,7 @@
 				{completedCount}/{$tasks.length}
 			</span>
 		</div>
-		<button on:click={() => $isModalOpen = true} class="absolute right-4 bg-themepicker duration-200 px-2 h-8 w-8 rounded">
+		<button on:click={() => $isModalOpen = true} class="absolute right-3 bg-themepicker duration-200 px-2 h-8 w-8 rounded">
 			<svg data-slot="icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 				<path clip-rule="evenodd" fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"></path>
 			</svg>
