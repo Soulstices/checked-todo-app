6096a7a5efbe93c0af4fa8a7cb1da1a4342a7c8a
Hennadii Bohdanov <soulstice.web@gmail.com>
2024-01-26 10:54:46 +0100

    Refactor Task component

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -30,16 +30,12 @@
 
 <div class="flex w-full">
 	<div
-		class="flex flex-row p-4 rounded-lg border form-check w-full transition duration-300"
-		class:mt-2={!$reverseTasksLayout}
-		class:mb-2={$reverseTasksLayout}
-		class:bg-task={!task.isCompleted}
-		class:border-task={!task.isCompleted}
-		class:bg-task-completed={task.isCompleted}
-		class:border-task-completed={task.isCompleted}
+		class="flex flex-row p-4 rounded-lg border form-check w-full
+		{task.isCompleted ? 'bg-task-completed border-task-completed' : 'bg-task border-task'}
+		{$reverseTasksLayout ? 'mb-2' : 'mt-2'}"
 	>
 		<input
-			class="form-check-input appearance-none h-4 w-4 border rounded-sm checked:after:m-[0.1rem] checked:after:ml-[0.275rem] checked:after:block checked:after:h-[0.5rem] checked:after:w-[0.35rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checkbox focus:outline-none align-top bg-no-repeat bg-center bg-contain float-left mr-3 ml-1 cursor-pointer scale-[1.4] mt-auto mb-auto transition duration-200"
+			class="form-check-input appearance-none h-4 w-4 border rounded-sm checked:after:m-[0.1rem] checked:after:ml-[0.275rem] checked:after:block checked:after:h-[0.5rem] checked:after:w-[0.35rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checkbox focus:outline-none align-top bg-no-repeat bg-center bg-contain float-left mr-3 ml-1 cursor-pointer scale-[1.4] mt-auto mb-auto"
 			type="checkbox"
 			bind:checked={task.isCompleted}
 			on:click={() => {
@@ -48,22 +44,15 @@
 			}}
 		/>
 		<span
-			class="inline-block pl-1 pr-2 flex-1 mt-auto mb-auto wrap-anywhere transition duration-300"
-			class:text-task={!task.isCompleted}
-			class:text-task-completed={task.isCompleted}
+			class="inline-block pl-1 pr-2 flex-1 mt-auto mb-auto wrap-anywhere
+			{task.isCompleted ? 'text-task-completed' : 'text-task'}"
 		>
 			{task.text}
 		</span>
 		<button
 			type="button"
-			class="mt-auto mb-auto
-		   text-remove hover:text-white
-
-		  inline-block rounded-full leading-normal shadow-md
-		  hover:shadow-lg focus:shadow-lg focus:outline-none border-none !outline-none focus:ring-0
-		  active:shadow-lg transition duration-200 ease-in-out w-9 h-9"
-			class:bg-btn-3={task.isCompleted}
-			class:bg-btn-2={!task.isCompleted}
+			class="mt-auto mb-auto text-remove hover:text-white inline-block rounded-full leading-normal shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none border-none !outline-none focus:ring-0 active:shadow-lg ease-in-out w-9 h-9 transition duration-200
+			 {task.isCompleted ? 'bg-btn-3' : 'bg-btn-2'}"
 			on:click={() => {
 				deleteTask()
 			}}
