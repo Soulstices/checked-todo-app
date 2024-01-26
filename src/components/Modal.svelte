22921f4c86ff5429f14a15f64d205fb19ac30e62
Hennadii Bohdanov <soulstice.web@gmail.com>
2024-01-26 09:07:55 +0100

    Make modal closeable by pressing "Esc" button

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -3,38 +3,50 @@
 	import { isModalOpen } from '../lib/store.js'
 	import Icon from './Icon.svelte'
 	import { IconType } from '../lib/types'
+	import { onDestroy, onMount } from 'svelte'
 
 	export let title: string
 	export let titleIcon: IconType
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.key === 'Escape') {
+			closeModal()
+		}
+	}
+
+	function closeModal() {
+		$isModalOpen = false
+	}
+
+	onMount(() => {
+		addEventListener('keydown', handleKeydown)
+	})
+
+	onDestroy(() => {
+		removeEventListener('keydown', handleKeydown)
+	})
 </script>
 
-{#if $isModalOpen}
-	<div class="relative z-40 visible tracking-normal" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-		<div class="fixed top-0 left-0 right-0 bottom-0 bg-neutral-900/70" transition:fade={{ duration: 400 }} />
-		<div class="fixed inset-0 z-10" transition:scale={{ duration: 400, start: 0.8 }}>
-			<div class="flex items-end justify-center p-0 lg:p-4 sm:items-center sm:p-0">
-				<div
-					class="relative transform top-0 lg:top-14 overflow-hidden lg:rounded-lg bg-modal shadow-lg my-0 lg:my-8 w-full lg:max-w-2xl h-screen lg:h-auto border border-modal transition duration-300"
-				>
-					<div class="p-0 pt-0 lg:rounded">
-						<div class="flex flex-row bg-modal-header pt-5 pb-5 pl-7 text-modal transition duration-300">
-							{#if titleIcon}
-								<Icon type={titleIcon} classNames="w-6 h-6 my-auto" />
-							{/if}
-							<h4 class="text-2xl font-bold uppercase tracking-wider my-auto mr-auto" class:pl-3={titleIcon}>{title}</h4>
-							<button
-								class="rounded-3xl bg-btn-2 p-3 mr-5 cursor-pointer duration-200 my-auto"
-								on:click={() => {
-									$isModalOpen = false
-								}}
-							>
-								<Icon type={IconType.Cross} />
-							</button>
-						</div>
-						<div class="p-7 pt-7 text-task"><slot /></div>
+<div class="relative z-40 visible tracking-normal" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+	<div class="fixed top-0 left-0 right-0 bottom-0 bg-neutral-900/70" transition:fade={{ duration: 400 }} />
+	<div class="fixed inset-0 z-10" transition:scale={{ duration: 400, start: 0.8 }}>
+		<div class="flex items-end justify-center p-0 lg:p-4 sm:items-center sm:p-0">
+			<div
+				class="relative transform top-0 lg:top-14 overflow-hidden lg:rounded-lg bg-modal shadow-lg my-0 lg:my-8 w-full lg:max-w-2xl h-screen lg:h-auto border border-modal transition duration-300"
+			>
+				<div class="p-0 pt-0 lg:rounded">
+					<div class="flex flex-row bg-modal-header pt-5 pb-5 pl-7 text-modal transition duration-300">
+						{#if titleIcon}
+							<Icon type={titleIcon} classNames="w-6 h-6 my-auto" />
+						{/if}
+						<h4 class="text-2xl font-bold uppercase tracking-wider my-auto mr-auto" class:pl-3={titleIcon}>{title}</h4>
+						<button class="rounded-3xl bg-btn-2 p-3 mr-5 cursor-pointer duration-200 my-auto" on:click={closeModal}>
+							<Icon type={IconType.Cross} />
+						</button>
 					</div>
+					<div class="p-7 pt-7 text-task"><slot /></div>
 				</div>
 			</div>
 		</div>
 	</div>
-{/if}
+</div>
--- src/components/StickyBar.svelte
+++ src/components/StickyBar.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onMount } from 'svelte'
 	import { tasks, isModalOpen, activeTabIndex, isTaskCreatorTooltipOpen, reverseTasksLayout } from '../lib/store.js'
 	import Modal from './Modal.svelte'
 	import Icon from './Icon.svelte'
 	import { IconType } from '../lib/types'
 
-	let isAtTop: boolean = true;
-	let isLinkCopied: boolean = false;
-	let completedCount: number;
+	let isAtTop: boolean = true
+	let isLinkCopied: boolean = false
+	let completedCount: number
 
 	export let saveInURL = () => {}
 
 	$: {
-		completedCount = $tasks.filter(item => item.isCompleted).length;
+		completedCount = $tasks.filter((item) => item.isCompleted).length
 	}
 
 	function scrollToTop(): void {
-		window.scroll(0, 0);
+		window.scroll(0, 0)
 	}
 
 	function updateScrollPosition() {
-		isAtTop = window.scrollY < 100;
+		isAtTop = window.scrollY < 100
 	}
 
 	function deleteAllTasks() {
 		$tasks = []
 
-		const settingsGlobal = localStorage.getItem('settings-global');
+		const settingsGlobal = localStorage.getItem('settings-global')
 		localStorage.clear()
 		if (settingsGlobal) {
 			localStorage.setItem('settings-global', settingsGlobal)
@@ -41,47 +41,68 @@
 	}
 
 	function handleShareBtnClick() {
-		navigator.clipboard.writeText(window.location.href)
-			.then(() => {
-				isLinkCopied = true
-				setTimeout(() => {isLinkCopied = false}, 3000)
-			})
+		navigator.clipboard.writeText(window.location.href).then(() => {
+			isLinkCopied = true
+			setTimeout(() => {
+				isLinkCopied = false
+			}, 3000)
+		})
 	}
 
 	onMount(() => {
-		window.addEventListener('scroll', updateScrollPosition);
-	});
+		window.addEventListener('scroll', updateScrollPosition)
+	})
 </script>
 
-<Modal title="Tasks Deletion" titleIcon={IconType.Trash}>
-	Do you really want to delete all your tasks?
-	<div class="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0 pt-5">
-		<button
-			class="flex items-center p-2 rounded w-full border-2 duration-200 bg-themepicker border-themepicker text-red-600"
-			on:click={deleteAllTasks}
-		>
-			<Icon type={IconType.Check}></Icon>
-			<span class="uppercase pl-3 pr-1 tracking-wider pt-[1px] font-bold">Yes, Delete All Tasks</span>
-		</button>
-		<button
-			class="flex flex-row items-center p-2 rounded w-full border-2 duration-200 bg-themepicker border-themepicker"
-			on:click={closeModal}
-		>
-			<Icon type={IconType.Cross}></Icon>
-			<span class="uppercase pl-3 pr-1 tracking-wider pt-[1px] text-task font-bold">No, Cancel</span>
-		</button>
-	</div>
-</Modal>
+{#if $isModalOpen}
+	<Modal title="Tasks Deletion" titleIcon={IconType.Trash}>
+		Do you really want to delete all your tasks?
+		<div class="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0 pt-5">
+			<button
+				class="flex items-center p-2 rounded w-full border-2 duration-200 bg-themepicker border-themepicker text-red-600"
+				on:click={deleteAllTasks}
+			>
+				<Icon type={IconType.Check}></Icon>
+				<span class="uppercase pl-3 pr-1 tracking-wider pt-[1px] font-bold">Yes, Delete All Tasks</span>
+			</button>
+			<button class="flex flex-row items-center p-2 rounded w-full border-2 duration-200 bg-themepicker border-themepicker" on:click={closeModal}>
+				<Icon type={IconType.Cross}></Icon>
+				<span class="uppercase pl-3 pr-1 tracking-wider pt-[1px] text-task font-bold">No, Cancel</span>
+			</button>
+		</div>
+	</Modal>
+{/if}
 
-{#if $tasks.length > 0 && $activeTabIndex === 0 }
-	<div class="fixed bottom-0 bg-container left-1/2 translate-x-[-50%] border-task flex w-full items-center py-4 text-task max-w-lg"
-		  class:z-30={!$isTaskCreatorTooltipOpen}
-		  class:z-10={$isTaskCreatorTooltipOpen}
-		  class:hidden={$isTaskCreatorTooltipOpen && $reverseTasksLayout}>
-		<progress value={completedCount} max={$tasks.length} class="absolute -top-1 right-0 left-0 w-full h-1" class:progress-green={completedCount === $tasks.length} class:progress-blue={completedCount !== $tasks.length}/>
+{#if $tasks.length > 0 && $activeTabIndex === 0}
+	<div
+		class="fixed bottom-0 bg-container left-1/2 translate-x-[-50%] border-task flex w-full items-center py-4 text-task max-w-lg"
+		class:z-30={!$isTaskCreatorTooltipOpen}
+		class:z-10={$isTaskCreatorTooltipOpen}
+		class:hidden={$isTaskCreatorTooltipOpen && $reverseTasksLayout}
+	>
+		<progress
+			value={completedCount}
+			max={$tasks.length}
+			class="absolute -top-1 right-0 left-0 w-full h-1"
+			class:progress-green={completedCount === $tasks.length}
+			class:progress-blue={completedCount !== $tasks.length}
+		/>
 
-		<button on:click={scrollToTop} class="absolute left-3 bg-themepicker duration-200 px-2 h-8 w-8 rounded" class:opacity-50={isAtTop} disabled={isAtTop}>
-			<svg data-slot="icon" fill="none" stroke-width="6" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+		<button
+			on:click={scrollToTop}
+			class="absolute left-3 bg-themepicker duration-200 px-2 h-8 w-8 rounded"
+			class:opacity-50={isAtTop}
+			disabled={isAtTop}
+		>
+			<svg
+				data-slot="icon"
+				fill="none"
+				stroke-width="6"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				aria-hidden="true"
+			>
 				<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5"></path>
 			</svg>
 		</button>
@@ -91,8 +112,12 @@
 				{completedCount}/{$tasks.length}
 			</span>
 		</div>
-		<button on:click={handleShareBtnClick} class="absolute right-[45px] bg-themepicker duration-200 px-2 h-8 w-[90px] rounded-l flex flex-row" disabled={isLinkCopied}>
-			<Icon type={IconType.Share} classNames="w-4 h-4 my-auto"/>
+		<button
+			on:click={handleShareBtnClick}
+			class="absolute right-[45px] bg-themepicker duration-200 px-2 h-8 w-[90px] rounded-l flex flex-row"
+			disabled={isLinkCopied}
+		>
+			<Icon type={IconType.Share} classNames="w-4 h-4 my-auto" />
 			<span class="m-auto pl-2 pr-1 uppercase text-xs font-bold">
 				{#if !isLinkCopied}
 					Share
@@ -101,9 +126,13 @@
 				{/if}
 			</span>
 		</button>
-		<button on:click={() => $isModalOpen = true} class="absolute right-3 bg-themepicker duration-200 px-2 h-8 w-8 rounded-r hover:text-red-600">
+		<button on:click={() => ($isModalOpen = true)} class="absolute right-3 bg-themepicker duration-200 px-2 h-8 w-8 rounded-r hover:text-red-600">
 			<svg data-slot="icon" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-				<path clip-rule="evenodd" fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"></path>
+				<path
+					clip-rule="evenodd"
+					fill-rule="evenodd"
+					d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+				></path>
 			</svg>
 		</button>
 	</div>
