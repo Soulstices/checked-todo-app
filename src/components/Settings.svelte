3ad0641ba04330f323d384cab8596ae9239c3664
Hennadii Bohdanov <soulstice.web@gmail.com>
2024-01-29 10:44:47 +0100

    Change order of elements in settings tab

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -9,6 +9,13 @@
 <div class="m-1">
 	<p class="font-bold text-xl ml-2 text-modal pb-5">Settings</p>
 
+	<p class="text-modal font-bold text-md ml-2 mb-1">Theme</p>
+	<div class="flex flex-col md:grid md:grid-cols-2 items-center text-lg font-bold gap-4 pb-4">
+		{#each themes as theme}
+			<ThemePicker newTheme={theme} />
+		{/each}
+	</div>
+
 	<SettingToggle
 		title="Smooth Scrolling"
 		description={`Clicking on the "Scroll to Top" or "Scroll to Bottom" buttons will smoothly scroll the page, instead of scrolling instantly.`}
@@ -20,12 +27,4 @@
 		description={`By default, new tasks are displayed at the top. When the option is enabled, new tasks will instead appear at the bottom.`}
 		settingName="useReversedLayout"
 	/>
-
-	<div class="flex flex-col md:grid md:grid-cols-2 items-center text-lg font-bold pt-1 gap-0"></div>
-	<p class="text-modal font-bold text-md mt-[-3px] ml-2">Theme</p>
-</div>
-<div class="flex flex-col md:grid md:grid-cols-2 items-center text-lg font-bold gap-4">
-	{#each themes as theme}
-		<ThemePicker newTheme={theme} />
-	{/each}
 </div>
